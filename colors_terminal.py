200cea6def9b86a1d3f6a8c2496e9f8d386bf600
saito <beatriz.s.gobira@gmail.com>
2022-08-01 17:28:47 -0400

    praticas

---
+++ colors_terminal.py
@@ -0,0 +1,14 @@
+print('\033[0;30;41mTeste')
+print('\033[4;33;44mTeste')
+print('\33[1;35;43mTeste')
+print('\33[30;42mTeste')
+print('\33[mTeste')
+print('\33[7:30mTeste')
+
+print('\033[0;30;41mTeste\33[m')
+print('\033[4;33;44mTeste\33[m')
+print('\33[1;35;43mTeste\33[m')
+print('\33[30;42mTeste\33[m')
+print('\33[mTeste\33[m')
+print('\33[7:30mTeste\33[m')
+#ultima aula do mundo 1
