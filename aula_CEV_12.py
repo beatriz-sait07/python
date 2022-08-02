1d1fb9aa220a0041d07e72aaca7abeb0ea328661
saito <beatriz.s.gobira@gmail.com>
2022-08-02 16:21:54 -0400

    pratica

---
+++ aula_CEV_12.py
@@ -0,0 +1,7 @@
+nome = input('qual seu nome? ').lower()
+if nome == 'gustavo':
+    print('que nome bonito')
+elif nome == 'maria':
+    print('nome popular do Brasil')
+else:
+    print('prazer em conhece-lo')
---
+++ ex_CEV_036.py
@@ -0,0 +1,9 @@
+valor_casa = float(input('valor da casa: '))
+salario = float(input('salario: '))
+tempo = int(input('pretende quitar a casa em quanto tempo (anos): '))
+mensalidade = valor_casa / (tempo * 12)
+print('R$ {:.2f} por mes'.format(mensalidade))
+if mensalidade > (salario - (30/100 * salario)):
+    print('emprestimo \033[1;31mNEGADO\033[m')
+else:
+    print('emprestimo \033[1;32mAPROVADO\033[m')
