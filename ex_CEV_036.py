cb8b75f5e0bdb3812dd31c8ea206788fa6420d15
saito <beatriz.s.gobira@gmail.com>
2022-08-03 16:09:17 -0400

    praticas

--- ex_CEV_036.py
+++ ex_CEV_036.py
@@ -3,7 +3,7 @@ salario = float(input('salario: '))
 tempo = int(input('pretende quitar a casa em quanto tempo (anos): '))
 mensalidade = valor_casa / (tempo * 12)
 print('R$ {:.2f} por mes'.format(mensalidade))
-if mensalidade > (salario - (30/100 * salario)):
-    print('emprestimo \033[1;31mNEGADO\033[m')
-else:
+if mensalidade < (salario - (30/100 * salario)):
     print('emprestimo \033[1;32mAPROVADO\033[m')
+else:
+    print('emprestimo \033[1;31mNEGADO\033[m')
--- ex_CEV_037.py
+++ ex_CEV_037.py
@@ -3,12 +3,10 @@ import math
 n = int(input('digite um numero: '))
 base = int(input('escolha um tipo de base: \nBinario -> (1), octal -> (2) ou hexadecimal -> (3)\n'))
 if base == 1:
-    conv = n % 2
-    print('a conversao escolhida foi para \033[1;36mBINARIO\033[m\nbase 10 = {}\nbase 2 = {}'.format(n,conv))
+    print('a conversao escolhida foi para \033[1;36mBINARIO\033[m\nbase 10 = {}\nbase 2 = {}'.format(n, bin(n))[2:])
 elif base == 2:
-    conv = n % 8
-    print('a conversao escolhida foi para \033[1;36mOCTADECIMAL\033[m base 10 = {}\nbase 8 = {}'.format(n, conv))
-
+    print('a conversao escolhida foi para \033[1;36mOCTADECIMAL\033[m base 10 = {}\nbase 8 = {}'.format(n, oct(n)[2:]))
 elif base == 3:
-    conv = n % 16
-    print('a conversao escolhida foi para \033[1;36mHEXADECIAL\033[m\nbase 10 = {}\nbase 16 = {}'.format(n, conv))
+    print('a conversao escolhida foi para \033[1;36mHEXADECIAL\033[m\nbase 10 = {}\nbase 16 = {}'.format(n, hex(n)[2:]))
+else:
+    print('numero invalido!')
