e9bbe1e2b7c453bc630bfea3299315eb4aa9e9ee
saito <beatriz.s.gobira@gmail.com>
2022-08-03 16:24:40 -0400

    praticas

--- ex_CEV_039.py
+++ ex_CEV_039.py
@@ -1,5 +1,12 @@
 from datetime import date
-nome = input('qual sua idade: ')
-idade = input('informe sua data de nascimento: ')
-atual = date.today().day
-
+atual = date.today().year
+nome = input('qual seu nome: ')
+idade = int(input('informe sua ano de nascimento: '))
+if (atual - idade) == 18:
+    print('voce tem que se aistar imadiatamente!')
+elif (atual - idade) < 18:
+    alist = 18 - (atual - idade)
+    print('falta {} anos para voce se alistar'.format(alist))
+else:
+    alist = (atual - idade) - 18
+    print('voce devia ter se alistado ha {} anos'.format(alist))
