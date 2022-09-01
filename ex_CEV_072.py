c69f6ee5ec5b116e5f9f6b50e5c9557172d60b80
saito <beatriz.s.gobira@gmail.com>
2022-09-01 18:59:04 -0400

    tupla/while True

---
+++ ex_CEV_072.py
@@ -0,0 +1,12 @@
+cont = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis','sete',
+        'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze',
+        'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+dados = int(input('digite um numero de 0 - 20: '))
+while True:
+    print('voce digitou um numero invalido')
+    tent = int(input('Digite novamente um numero entre 0 - 20: '))
+    if tent < 0 or tent > 20:
+        break
+print('\ndigito valido!\n')
+print(cont[dados])
+
