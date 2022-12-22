f3d0618919205b364392832e815d49b7250fca81
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2022-12-21 23:29:17 -0400

    dict, if

--- curso_em_video/exercicios/ex_CEV_090.py
+++ curso_em_video/exercicios/ex_CEV_090.py
@@ -1,7 +1,7 @@
 estudantes = {'nome': input('nome do aluno: '), 'nota': float(input('nota: '))}
 print('-='*30)
 
-if estudantes['nota'] < 5:
+if estudantes['nota'] <= 5:
     print(f'Nome: {estudantes["nome"]}\nMedia: {estudantes["nota"]}\nSituacao: Reprovado\n')
 elif estudantes['nota'] > 5 and estudantes['nota'] < 7:
     print(f'Nome: {estudantes["nome"]}\nMedia: {estudantes["nota"]}\nSituacao: Exame, precisando de {7 - estudantes["nota"]}\n')
