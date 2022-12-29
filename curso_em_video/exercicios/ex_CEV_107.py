e21e9ac1e5295cc37434d54ef429b92bf279ab52
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2022-12-29 13:44:45 -0400

    modularizacao

--- curso_em_video/exercicios/ex_CEV_107.py
+++ curso_em_video/exercicios/ex_CEV_107.py
@@ -3,4 +3,4 @@ import modularizacao
 valor = float(input('Digite um valor: '))
 print(f'a metade de R$ {valor} eh R$ {modularizacao.metade(valor)}')
 print(f'o dobro de R$ {valor} eh de {modularizacao.dobro(valor)}')
-print(f'aumentando 10%, em R$ {valor} teremos R$ {modularizacao.aumento(valor)}')
+print(f'aumentando 10%, em R$ {valor} teremos R$ {modularizacao.aumento(valor, 10)}')
