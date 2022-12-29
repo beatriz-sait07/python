45a365dcf2adbd8da70a5ac7b9a72f63d898ad5f
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2022-12-29 02:25:21 -0400

    implementacao da modularizacao

---
+++ curso_em_video/aulas/aulas_apartir_22/modularizacao_inicio.py
@@ -0,0 +1,10 @@
+#aula teorica
+import uteis
+
+num=int(input('digite um valor: '))
+fat = uteis.fatorial(num)
+print(f'o fatorial de {num} eh {fat}')
+print(f'o dobro de {num} eh {uteis.dobro(num)}')
+print(f'o triplo de {num} eh {uteis.triplo(num)}')
+
+
