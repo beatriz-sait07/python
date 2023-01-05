a8b64c590d6d38d62aa534964b5f3a585d71effa
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2023-01-04 22:33:41 -0400

    ex112_pacote/modularizacao

---
+++ curso_em_video/exercicios/ex111/utilidade/dado/__init__.py
@@ -0,0 +1,9 @@
+def leiaMoney (msg):
+    valido = False
+    while not valido:
+        entrada = str(input(msg)).replace(',', '.').strip()
+        if entrada.isalpha() or entrada == '':
+            print(f'\033[0;31mERRO: \"{entrada}\" eh um preco invalido!\033[m')
+        else:
+            valido = True
+            return float(entrada)
