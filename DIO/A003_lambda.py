d61b9ff5dba2e4c1892bf95a89118133d2782cc9
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2023-01-22 22:01:46 -0400

    lambda

---
+++ DIO/A003_lambda.py
@@ -0,0 +1,10 @@
+cont_letras = lambda lista: [len(x) for x in lista]
+
+lista_animais = ['dog', 'cats', 'duck']
+print(cont_letras(lista_animais))
+
+soma = lambda a, b: a + b
+sub = lambda a, b: a - b
+
+print(soma(5, 10))
+print(sub(91, 6))
