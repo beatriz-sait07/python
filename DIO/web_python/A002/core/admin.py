777d4beb2d5585d8da2f0decc01d2302005ab656
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2023-01-31 00:11:34 -0400

    add filtos de pesquisa

--- DIO/web_python/A002/core/admin.py
+++ DIO/web_python/A002/core/admin.py
@@ -4,6 +4,7 @@ from core.models import Eventos
 #enfeitando a visualização
 class Evento_adm(admin.ModelAdmin):
     list_display = ('titulo', 'data_evento', 'data_criação')
+    list_filter = ('usuario','data_evento')
 
 #registar class/models
 admin.site.register(Eventos, Evento_adm)
