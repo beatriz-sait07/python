1acab748097aec38996d99862870989f665e5fc5
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2023-01-30 23:58:16 -0400

    puxando banco de dados, e 'enfeitando' o adm

---
+++ DIO/web_python/A002/core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from core.models import Eventos
+
+#enfeitando a visualização
+class Evento_adm(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criação')
+
+#registar class/models
+admin.site.register(Eventos, Evento_adm)
