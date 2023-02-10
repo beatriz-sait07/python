228126c001eb053cfe953bbde68dc1afa7eb9223
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2023-02-10 19:17:06 -0400

    do core no app, makemigrations do core

--- DIO/web_python/A002/core/models.py
+++ DIO/web_python/A002/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100) #limite maximo de caracter
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField()
+    data_criacao = models.DateTimeField(auto_now=True) #feito para que o usuario nao modifique o mesmo, e ele eh gerado no momento da criacao do evento
+    
+    
