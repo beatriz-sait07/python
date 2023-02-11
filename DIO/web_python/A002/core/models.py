96b8daede23ed5b5e8bbe3026d6445c9cac5e911
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2023-02-10 20:19:09 -0400

    migrate 0002, add filter

--- DIO/web_python/A002/core/models.py
+++ DIO/web_python/A002/core/models.py
@@ -1,15 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Evento(models.Model):
     titulo = models.CharField(max_length=100) #limite maximo de caracter
     descricao = models.TextField(blank=True, null=True)
-    data_evento = models.DateTimeField(verbose_name='Dara do evento')
+    data_evento = models.DateTimeField(verbose_name='Data do evento')
     data_criacao = models.DateTimeField(auto_now=True) #feito para que o usuario nao modifique o mesmo, e ele eh gerado no momento da criacao do evento
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) #cascade serve para deletar TODOS os dados do usuario caso ele seja removido da lista
     
     class Meta:
         db_table = 'evento'
         
     def __str__(self):
         return self.titulo
+    
+    #a cada modificacao de classe, tu precisa migrar o banco de dados!
