8b5431a3d38c54915313d2cfc38eca79d8462163
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2023-02-18 09:19:54 -0400

    autenticacao pelo usuario-correcao de erro da pag

--- DIO/web_python/A002/core/views.py
+++ DIO/web_python/A002/core/views.py
@@ -30,8 +30,9 @@ def submit_login(request):
     
 def lista_eventos(request):
     #evento = Evento.objects.get(id=1) --> busca apenas um elemento 'aquele que foi requesitado'
+    user = request.user
     usuario = request.user
-    evento = Evento.objects.all() # faz uma listagem dos objetos
-    #evento = Evento.objects.filter(usuario=usuario) # filta na pagina html a aparecer apenas os dados do usuario que esta logado no momento
+    #evento = Evento.objects.all() # faz uma listagem dos objetos
+    evento = Evento.objects.filter(usuario=usuario) # filta na pagina html a aparecer apenas os dados do usuario que esta logado no momento
     response = {'eventos': evento}
     return render(request, 'agenda.html', response) #renderizar a pagina html
