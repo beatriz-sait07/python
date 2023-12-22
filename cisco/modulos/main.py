e04343e55f2d8caa94426ab792440cb99fe775a2
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2023-12-22 15:45:48 -0400

    secao 1.3 cisco, testando

--- cisco/modulos/main.py
+++ cisco/modulos/main.py
@@ -1,1 +1,7 @@
 import mod1, mod2
+
+if __name__ == "__main__":
+    print("rodando mod1")
+else:
+    print("rodando mod2")
+#aqui somente o mod1 esta sendo importado, pois o python esta omitindo silenciosamente as importações subsequentes 
