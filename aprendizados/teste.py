4f63bfd24015352e516d3514578712ff860ea086
beatriz_sait07 <beatriz.s.gobira@gmail.com>
2023-12-22 15:23:56 -0400

    testando importações

---
+++ aprendizados/teste.py
@@ -0,0 +1,19 @@
+"""modelo_base = ['A', 'A', 'A', 'A', 'A', 'A']
+lista3 = []
+count = 0
+
+for i in range(10):
+    for j in range(10):
+        modelo = modelo_base.copy()
+        modelo[3] = str(i)
+        modelo[5] = str(j)
+        lista3.append(''.join(modelo))
+        count += 1
+
+print(lista3)
+print(count)
+------------------------------------------------------------"""
+
+
+
+
